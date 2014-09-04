c3b1631767bacd3ce9cd7ea38b536a83873ea6a6
Dan Condurache <dan.condurache@softvision.ro>
2014-09-04 12:53:00 +0300

    Modified the run_test_suite_2
    1. Removed unnecessary imports
    2. Gave the method a more python-esque name

--- run_test_suite_2.py
+++ run_test_suite_2.py
@@ -1,18 +1,12 @@
-
 import time
 import unittest
-from selenium import webdriver
-from BaseTest import BaseTest
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoSuchAttributeException
 from DemoTest import TestSuite1
-import sys, getopt
 
-def suite110(self):
+def test_suite_2(self):
 #     Runs all the tests from the TestSuite1 class
 	print 'Runs the tests from 1 to 10'
-	suite110 = unittest.TestSuite()
-	suite110.addTest(unittest.makeSuite(TestSuite1))
-	return suite110
+	test_suite_2 = unittest.TestSuite()
+	test_suite_2.addTest(unittest.makeSuite(TestSuite1))
+	return test_suite_2
 
-unittest.TextTestRunner(verbosity=9).run(suite110(2))
+unittest.TextTestRunner(verbosity=9).run(test_suite_2(2))
