52fcfcac21bd1ea97b696ff11bf97321b0869768
Dan Condurache <dan.condurache@softvision.ro>
2014-09-04 12:52:21 +0300

    Modified run_test_suite_1
    1. Removed the unnecessary imports
    2. Gave the method a more python-esque name

--- run_test_suite_1.py
+++ run_test_suite_1.py
@@ -1,19 +1,15 @@
 import time
 import unittest
-from selenium import webdriver
-from BaseTest import BaseTest
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoSuchAttributeException
 from DemoTest import TestSuite1
-import sys, getopt
 
-def suite135(self):
+
+def test_suite_1(self):
 #     Runs tests 1,3,5  from TestSuite1
 	print 'Runs tests 01, 03, 05'
-    suite135 = unittest.TestSuite()
-    suite135.addTest(TestSuite1('test_01_try_memeo_modal_is_displayed'))
-    # suite135.addTest(TestSuite1('test_03_learn_more_is_displayed_in_delete_user_modal'))
-    # suite135.addTest(TestSuite1('test_05_website_footer_is_displayed'))
-    return suite135
+	test_suite_1 = unittest.TestSuite()
+	test_suite_1.addTest(TestSuite1('test_01_try_memeo_modal_is_displayed'))
+	test_suite_1.addTest(TestSuite1('test_03_learn_more_is_displayed_in_delete_user_modal'))
+	test_suite_1.addTest(TestSuite1('test_05_website_footer_is_displayed'))
+	return test_suite_1
 
-unittest.TextTestRunner(verbosity=2).run(suite135(2))
+unittest.TextTestRunner(verbosity=2).run(test_suite_1(2))
