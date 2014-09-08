9316732a9b0d73c322aafb3d20db2a81e5697f59
Dan Condurache <dan.condurache@softvision.ro>
2014-09-08 15:22:07 +0300

    Modified test suite 1 & 2
    modified run_test_suite1 & 2 to use the newly implemented HTML Runner
    method

--- run_test_suite_1.py
+++ run_test_suite_1.py
@@ -1,15 +1,24 @@
 import time
 import unittest
 from DemoTest import TestSuite1
+import HTMLTestRunner
 
 
 def test_suite_1(self):
-#     Runs tests 1,3,5  from TestSuite1
-	print 'Runs tests 01, 03, 05'
 	test_suite_1 = unittest.TestSuite()
 	test_suite_1.addTest(TestSuite1('test_01_try_memeo_modal_is_displayed'))
 	test_suite_1.addTest(TestSuite1('test_03_learn_more_is_displayed_in_delete_user_modal'))
 	test_suite_1.addTest(TestSuite1('test_05_website_footer_is_displayed'))
 	return test_suite_1
 
-unittest.TextTestRunner(verbosity=2).run(test_suite_1(2))
+def run(test_suite_1, report = "test_results.html"):
+	with open(report, "w") as f:
+		HTMLTestRunner.HTMLTestRunner(
+					stream = f,
+					title = 'Test Suite 1',
+					verbosity = 2,
+					description = 'Runs tests 1,3,5  from TestSuite1'
+					).run(test_suite_1)
+
+if __name__ == "__main__":
+	run(test_suite_1(2))
--- run_test_suite_2.py
+++ run_test_suite_2.py
@@ -1,12 +1,21 @@
 import time
 import unittest
 from DemoTest import TestSuite1
+import HTMLTestRunner
 
 def test_suite_2(self):
-#     Runs all the tests from the TestSuite1 class
-	print 'Runs the tests from 1 to 10'
 	test_suite_2 = unittest.TestSuite()
 	test_suite_2.addTest(unittest.makeSuite(TestSuite1))
 	return test_suite_2
 
-unittest.TextTestRunner(verbosity=9).run(test_suite_2(2))
+def run(test_suite_2, report = "Results/test_results.html"):
+	with open(report, "w") as f:
+		HTMLTestRunner.HTMLTestRunner(
+					stream = f,
+					title = 'Test Suite 1',
+					verbosity = 2,
+					description = 'Runs the tests from 1 to 10'
+					).run(test_suite_2)
+
+if __name__ == "__main__":
+	run(test_suite_2(2))
